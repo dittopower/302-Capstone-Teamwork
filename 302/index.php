2dfd899c5f1e156830057f521d787bbbf6ced41d
roflmonsterjh <roflmonster.jh@gmail.com>
2015-08-28 16:19:49 +1000

    Basic layout
    Created basic layout
    various styling
    example page

--- 302/index.php
+++ 302/index.php
@@ -1,12 +1,49 @@
-<?php
-	require_once "lib.php";
-	lib_perms();
-	lib_login();
+<HTML>
+
+<HEAD>
+	<title>Teamwork</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</HEAD>
+<BODY>
+	<aside><!-- sidebar -->
+		<h2>Styled things m8</h2>
+		<br><br>
+		<input type="button" value="Press me" class="button button1">
+		<br><br>
+		<input type="button" value="Press me" class="button button2">
+		<br><br>
+		<input type="button" value="Press me" class="button button3">
+		
+		<br><br>
+		<input type="text" placeholder="Type text here" class="textinput">
+	</aside>
 	
-	if(isUser()){
-		echo "Hi, $_SESSION[name]";
-	}else{
-		echo "this site..";
-	}
-	user_form();
-?>
+	<nav>
+		<ul>
+			<li><a href="./">Home</a></li>
+			<li><a href="./">Not Home</a></li>
+			<li><a href="./">Not Home 3</a></li>
+		</ul>
+	</nav>
+	
+	<main>
+		<h1>Content Mayyyyneeee</h1>
+		
+		<p>This is where the feed/filesharing/teamwork stuff goes</p>
+		
+		<p>Ugh Blue Bottle four dollar toast cornhole, retro crucifix sriracha. Fashion axe tattooed hoodie normcore, four loko chambray post-ironic. Twee 8-bit Marfa mustache, chambray farm-to-table Kickstarter meggings cliche squid. Meditation mumblecore seitan, meh Williamsburg bitters VHS vegan Helvetica church-key High Life. Keffiyeh Echo Park Pitchfork, literally flannel stumptown try-hard cronut. Flannel Vice artisan direct trade, tote bag butcher swag vegan lumbersexual. Pork belly Carles Marfa chambray, disrupt mumblecore ugh direct trade tilde McSweeney's pug Vice banjo biodiesel skateboard.</p>
+
+		<p>VHS paleo quinoa, kogi kitsch beard High Life PBR&B. Selfies blog semiotics pug artisan. Gluten-free tilde raw denim, chia Marfa disrupt asymmetrical cray PBR. Deep v four dollar toast occupy Tumblr. Freegan Banksy American Apparel, pork belly tote bag crucifix ethical Bushwick. Sustainable forage art party Echo Park DIY, gentrify church-key yr roof party mumblecore YOLO four dollar toast pour-over taxidermy. Gluten-free sriracha roof party post-ironic keffiyeh.</p>
+
+		<p>Marfa dreamcatcher mumblecore semiotics fingerstache Shoreditch. Ennui Godard Marfa Schlitz. Chillwave occupy Etsy, Schlitz meh Neutra banh mi wolf umami fashion axe letterpress lomo ethical. Intelligentsia synth VHS before they sold out, +1 roof party plaid. Etsy tattooed Truffaut Godard, whatever salvia ennui sartorial Thundercats iPhone Pitchfork Echo Park. Cray fap selvage keffiyeh, health goth heirloom retro actually VHS listicle next level street art. Butcher yr literally messenger bag.</p>
+
+		<p>Carles irony sustainable tilde. VHS 8-bit Thundercats irony XOXO, Intelligentsia roof party Vice pour-over. Tumblr typewriter mumblecore sartorial XOXO Austin. Kale chips biodiesel Schlitz flexitarian, jean shorts gluten-free photo booth gentrify small batch bespoke Truffaut. Selvage direct trade crucifix food truck seitan, Godard 3 wolf moon Carles Shoreditch mumblecore gastropub roof party. Beard retro jean shorts, keffiyeh mixtape sustainable mumblecore Vice cardigan McSweeney's squid 8-bit irony fap. Authentic wayfarers mumblecore aesthetic.</p>
+		
+		<footer>
+			idk if we need a footer<br><br><br>
+			Joshua Henley | Damon Jones | Emma Jackson | Will Jakobs | Declan Winter
+		</footer>
+		
+	</main>
+</BODY>
+</HTML>
