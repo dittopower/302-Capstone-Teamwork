a98a5fdf8bada9be04105740825b70bd38a6a68c
dittopower <dittopower@gmail.com>
2015-09-22 20:54:25 +1000

    Using cards on the index

--- 302/index.php
+++ 302/index.php
@@ -1,22 +1,19 @@
 <?php
 	require_once "$_SERVER[DOCUMENT_ROOT]/lib.php";
 	page();
+	
+	echo "<h1>Select a group:</h1>";
+		$sql = "SELECT g.`GroupId`,`GroupName`,`GroupProject`,`UnitCode` FROM `Groups` g join Group_Members m on g.`GroupId` = m.GroupId WHERE m.UserId = '$_SESSION[person]'";
+		debug($sql);
+		$result = multiSQL($sql);
+		while($row = mysqli_fetch_array($result,MYSQL_ASSOC)){?>
+			<div class="card">
+				<h3><?php echo "$row[GroupName]";?></h3><hr>
+				Unit: <?php echo "$row[UnitCode]";?><br>
+				Project: <?php echo "$row[GroupProject]";?><br>
+				<a href='<?php echo "http://$_SERVER[HTTP_HOST]/group?group=$row[GroupId]"; ?>'><input class='button button1' type='button' value='Open'></a>
+			</div>
+	<?php
+		}
+		echo "<hr><h1><a href='http://$_SERVER[HTTP_HOST]/group/find'>Find a Group</a></h1>";
 ?>
-
-	<div class="card">
-		<h3>title 1</h3><hr>
-		testing a card<br><br><br>mdsjfldsfkjdslfk<br><br>dslfkjdslkfj
-		<input class='button button1' type='button' value='Do Something'>
-	</div>
-	
-	<div class="card">
-		<h3>title 2</h3><hr>
-		testing a card<br><br>mdsjfldsfkjdslfk<br><br>dslfkjdslkfj
-		<input class='button button1' type='button' value='Do Something'>
-	</div>
-	
-	<div class="card">
-		<h3>title 3</h3><hr>
-		testing a card<br>mdsjfldsfkjdslfk<br><br>dslfkjdslkfj
-		<input class='button button1' type='button' value='Do Something'>
-	</div>
