456339c040bd968e4ef4d9260e7f2c992c14f89f
dittopower <dittopower@gmail.com>
2015-09-22 19:24:14 +1000

    Moved group summary
    move to group homepage.
    now uses Group_selected()
    dates now in system and shown.

--- 302/group/index.php
+++ 302/group/index.php
@@ -1,23 +1,36 @@
 <?php
 	require_once "$_SERVER[DOCUMENT_ROOT]/lib.php";
 	page();
+	lib_login();
 	lib_group();
-?>
-<div class='members'>
-<?php var_dump(members());
-
-function members_i($group = ""){
-		if($group == ""){
-			$group = $_SESSION['group'];
+	
+	
+		group_selected();
+		
+		echo "<h2>Group Members</h2>";
+		
+		$memberids = members_id($group);
+		$i = 0;
+		foreach(members($group) as $item){
+			echo "#" . $memberids[$i] . ": " . $item . "<br>";
+			$i++;
 		}
-		$result = multiSQL("SELECT UserId as name FROM `Group_Members` g WHERE UserId != '$_SESSION[person]' and `GroupId` = '$group'");
-		$text = [];
-		while($row = mysqli_fetch_array($result,MYSQL_ASSOC)){
-			$text[] = $row['name'];
-		}
-		return $text;
-	}
-	var_dump(members_id());
-?>
 
-</div>
+		echo "<br><h2>Your Project Details</h2>";
+		
+		$thing = rowSQL("SELECT Name, ProjectType1, ProjectType2, ProjectType3, Description, skill, requirements, UnitCode, Supervisor, DATE_FORMAT(`Start`, '%a %d %b %Y') as start, DATE_FORMAT(`End`, '%a %d %b %Y') as end FROM Projects WHERE P_Id=(SELECT GroupProject FROM Groups WHERE GroupId='$_SESSION[group]')");
+		
+		echo "Project Title: <strong>" . $thing["Name"] . "</strong>";
+		echo "<br>Project Duration: <strong>".$thing['start']." to ".$thing['end']."</strong>";
+		echo "<br>Project Description: <strong>" . $thing["Description"] . "</strong>";
+		echo "<br>Project Requirements: <strong>" . $thing["requirements"] . "</strong>";
+		
+		echo "<br>";
+		echo "<br>Project Type: <strong>" . $thing["ProjectType1"] .", ". $thing["ProjectType2"] .", ". $thing["ProjectType3"] . "</strong>";
+		echo "<br>Skills required: <strong>" . $thing["skill"] . "</strong>";
+		
+		echo "<br>";
+		echo "<br>For Unit: <strong>" . $thing["UnitCode"] . "</strong>";
+		
+		echo "<br>With supervisor: <strong>" . $thing["Supervisor"] . "</strong>";
+?>
