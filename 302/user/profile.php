f7ce55aac44392b518d78dca7ccce52acc4a0a82
dittopower <dittopower@gmail.com>
2015-10-07 20:08:20 +1000

    Profiles v1.5
    Because reasons we have to emails in our system. So the one in the
    accounts table is from now on the qut email and the user details one is
    the users prefered/external email.
    
    Both are now shown on profiles

--- 302/user/profile.php
+++ 302/user/profile.php
@@ -5,17 +5,17 @@
 	//Get data
 
 	if(isset($_GET['u'])){
-		$sql= mysqli_prepare($mysqli, "SELECT GPA,Skills,Blurb,LinkedIn,D_Accounts.Email,Facebook,Skype,Phone,FirstName,LastName,Username FROM `deamon_INB302`.`User_Details` INNER JOIN D_Accounts ON D_Accounts.UserId=User_Details.UserId WHERE Username = ?");
+		$sql= mysqli_prepare($mysqli, "SELECT GPA,Skills,Blurb,LinkedIn,User_Details.Email,D_Accounts.Email as Qemail,Facebook,Skype,Phone,FirstName,LastName,Username FROM `deamon_INB302`.`User_Details` INNER JOIN D_Accounts ON D_Accounts.UserId=User_Details.UserId WHERE Username = ?");
 		mysqli_stmt_bind_param($sql,"s",$_GET['u']);
 	}else if(! isset($_SESSION['SupervisorID'])){
-		$sql= mysqli_prepare($mysqli, "SELECT GPA,Skills,Blurb,LinkedIn,D_Accounts.Email,Facebook,Skype,Phone,FirstName,LastName,Username FROM `deamon_INB302`.`User_Details` INNER JOIN D_Accounts ON D_Accounts.UserId=User_Details.UserId WHERE User_Details.UserId = ?");
+		$sql= mysqli_prepare($mysqli, "SELECT GPA,Skills,Blurb,LinkedIn,User_Details.Email,D_Accounts.Email as Qemail,Facebook,Skype,Phone,FirstName,LastName,Username FROM `deamon_INB302`.`User_Details` INNER JOIN D_Accounts ON D_Accounts.UserId=User_Details.UserId WHERE User_Details.UserId = ?");
 		mysqli_stmt_bind_param($sql,"s",$_SESSION['person']);
 	}
 	
 	if(isset($_GET['u']) || (! isset($_SESSION['SupervisorID']))){
 	
 		mysqli_stmt_execute($sql);
-		mysqli_stmt_bind_result($sql,$GPA,$Skills,$Blurb,$LinkedIn,$Email,$Facebook,$Skype,$Phone,$FirstName,$LastName,$Username);
+		mysqli_stmt_bind_result($sql,$GPA,$Skills,$Blurb,$LinkedIn,$Email,$Qemail,$Facebook,$Skype,$Phone,$FirstName,$LastName,$Username);
 		$result = $sql->store_result();
 		
 		//Display data
@@ -28,7 +28,8 @@
 			echo "<h1>Profile: $FirstName $LastName</h1>";
 			echo "<h2>ID: $Username</h2>";
 			
-			$cardcontent .=  "<b>Email:</b> <a href='mailto:$Email'>$Email</a>";
+			$cardcontent .=  "<b>QUT Email:</b> <a href='mailto:$Qemail'>$Qemail</a>";
+			$cardcontent .=  "<br><b>Contact Email:</b> <a href='mailto:$Email'>$Email</a>";
 			$cardcontent .=  "<br /><b>Phone:</b> <a href='tel:$Phone'>$Phone</a>";
 			$cardcontent .=  "<br /><b>Skype:</b> <a href='skype:$Skype?chat'>$Skype</a>";
 			$cardcontent .=  "<br /><b>LinkedIn:</b> <a href='https://www.linkedin.com/profile/view?id=$LinkedIn'>LinkedIn Profile</a>";
