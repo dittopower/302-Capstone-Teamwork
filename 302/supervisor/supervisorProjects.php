a2c4f20176d16ab861a11f4dfe209dfb5d205d3c
roflmonsterjh <roflmonster.jh@gmail.com>
2015-10-13 23:23:05 +1000

    Applications + more
    Some supervisor visual stuff
    applications can be accepted / declined
    applications have more stuff on them
    ****groups with applications accepted by more than 1 different
    supervisor will have weird group allocation stuff TOFIX

--- 302/supervisor/supervisorProjects.php
+++ 302/supervisor/supervisorProjects.php
@@ -4,38 +4,98 @@
 	
 	if(isset($_POST["appAction"])){//accept or deny applications
 		
-		echo "<h2>Application decision</h2>";
-		echo $_POST["appAction"] . "<br><br>" . $_POST["appid"];
+		/*echo "<h2>Application decision</h2>";
+		echo $_POST["appAction"] . "<br><br>" . $_POST["appid"];*/
 		
+		$type = $_POST["appAction"];
+		
+		if($type=="Accept"){ $type .= 'e'; }
+		
+		$appid = $_POST["appid"];//id of the project application
+		
+		$val = rowSQL("SELECT GroupId, P_Id FROM Project_Applications WHERE ApplicationID=" . $appid);
+		
+		$gid = $val["GroupId"];//group id
+		$pid = $val["P_Id"];//project id
+				
+		$q1 = runSQL("UPDATE Project_Applications SET Status='".$type."d' WHERE ApplicationID=" . $appid);
+		
+		if(!$g1){ echo "Project_Applications field updated.<br>"; }
+		else{ echo "Something went wrong.<br>"; }
+		
+		if($type == "Accept"){
+			$q2 = runSQL("UPDATE Groups SET GroupProject='".$pid."' WHERE GroupId=".$gid);
+			if(!$g2){ echo "Groups field updated."; }
+			else{ echo "Something went wrong."; }
+		}
+		else{//if the project is declined after already being accepted
+			$oldproject = singleSQL("SELECT GroupProject FROM Groups WHERE GroupId=".$gid);
+			if($oldproject == $pid){//if this was their project previously
+				$q2 = runSQL("UPDATE Groups SET GroupProject='0' WHERE GroupId=".$gid);
+				if(!$g2){ echo "Reverted to no project."; }
+				else{ echo "Something went wrong."; }
+			}
+		}
+		
+		echo "<br><a href='supervisorProjects.php'>Back.</a>";
+
 	}
 	else if(isset($_POST["viewapplication"])){
 		
 		$appid = $_POST["viewapplication"];
 		
-		echo "<h2>Application #".$appid."</h2>";
+		echo "<span class='nospace'><h2>Application #".$appid."</h2>";
 		
 		$appRows = rowSQL("SELECT * FROM Project_Applications WHERE ApplicationID=".$appid);
 		
-		echo "<br>Group: " . singleSQL("SELECT GroupName FROM Groups WHERE GroupId=" . $appRows["GroupId"]);
-		echo "<br>Project #".  $appRows["P_Id"] .": " . singleSQL("SELECT Name FROM Projects WHERE P_Id=" . $appRows["P_Id"]);
+		echo "<h3>Group:</h3> " . singleSQL("SELECT GroupName FROM Groups WHERE GroupId=" . $appRows["GroupId"]);
+		echo "<br><h3>Project #".  $appRows["P_Id"] .":</h3> " . singleSQL("SELECT Name FROM Projects WHERE P_Id=" . $appRows["P_Id"]);
 		
 		$groupMembers = arraySQL("SELECT CONCAT(`FirstName`,' ',`LastName`) AS name, a.Username AS username FROM `D_Accounts` a JOIN `Group_Members` g WHERE g.`UserId` = a.`UserId` and `GroupId`=".$appRows["GroupId"]);
 		
-		echo "<br>Group Members:<br>";
+		/* members listing */
+		echo "<br><h3>Group Members:</h3><br>";
 		
 		echo "<ul class='indent'>";
 			foreach($groupMembers as $person){
-				echo "<li><a href='/user/?u=".$person['username']."'>".$person['name']."</a></li>";
+				echo "<li><a href='/user/?u=".$person['username']."' target='_blank'>".$person['name']."</a></li>";
 			}
 		echo "</ul>";
 		
-		echo "<br><br>Team Skills: ____";
-		echo "<br>Cover Letter: " . $appRows["CoverLetter"];
-		echo "<br><br>Status: " . $appRows["Status"];
-		echo "<br>Time Submitted: " . date('j/n/y g:ia', strtotime($appRows["TimeSubmitted"]));
+
+		/* Skills listing */
+		echo "<br><h3>Team Skills:</h3></br>";
+		
+		$groupSkills = arraySQL("SELECT Skills FROM User_Details u LEFT JOIN Group_Members g ON g.UserId=u.UserId WHERE g.GroupId=".$appRows["GroupId"]);
+		
+		$allskills = "";
+		
+		foreach($groupSkills as $skillset){
+			$allskills .= $skillset["Skills"] . ",";
+		}
+		
+		$skillArray = array_unique(explode(',', $allskills));
+		$referenceArray=array();
+		
+		echo "<ul class='indent'>";
+		foreach($skillArray as $sk){
+			
+			if(!in_array(strtolower(trim($sk)), $referenceArray)) {
+				array_push($referenceArray, strtolower(trim($sk)));
+				
+				if(!trim($sk) == ""){
+					echo "<li>" . trim($sk) . "</li>";
+				}
+			}
+		}
+		echo "</ul><br>";
+		
+		echo "<br><h3>Cover Letter:</h3><br>" . $appRows["CoverLetter"];
+		echo "<br><br><h3>Status:</h3> " . $appRows["Status"];
+		echo "<br><h3>Time Submitted:</h3> " . date('j/n/y g:ia', strtotime($appRows["TimeSubmitted"]));
 		
 		echo "<br><br><form action='' method='post'><input type='hidden' name='appid' value='".$appid."'><input name='appAction' type='submit' value='Accept' class='button button1'></form><br>";
-		echo "<form action='' method='post'><input type='hidden' name='appid' value='".$appid."'><input name='appAction' type='submit' value='Decline' class='button button1'></form>";
+		echo "<form action='' method='post'><input type='hidden' name='appid' value='".$appid."'><input name='appAction' type='submit' value='Decline' class='button button1'></form></span>";
 		
 	}
 	else{
@@ -77,7 +137,18 @@
 			$apps = arraySQL("SELECT * FROM Project_Applications WHERE P_Id=" . $thing["P_Id"]);
 			foreach($apps as $single){
 				echo "<li><div class='left'>#" . $single["ApplicationID"] . "</a> - " . substr($single["CoverLetter"], 0, 50) . "...<br>" . date('j/n/y g:ia', strtotime($single["TimeSubmitted"]));
-					echo "<form action='' method='post'><input type='hidden' name='viewapplication' value='".$single["ApplicationID"]."'><input type='submit' value='View Application' class='button button1'></form><br>";
+					echo "<form action='' method='post'><input type='hidden' name='viewapplication' value='".$single["ApplicationID"]."'>";
+					
+					if($single["Status"]!="Applied"){//shows the supervisor their previous decisions
+						echo "<input type='submit' value='(".$single["Status"].") View Application'";
+						if($single["Status"]=="Accepted"){ echo "class='button button3'>"; }
+						else if($single["Status"]=="Declined"){ echo "class='button button2'>"; }
+						else{ echo "class='button button1'>"; }
+					}else{
+						echo "<input type='submit' value='View Application' class='button button1'>";
+					}
+					
+					echo "</form><br>";
 				echo "</li>";//fix the floating things (clear)
 				
 			}			
