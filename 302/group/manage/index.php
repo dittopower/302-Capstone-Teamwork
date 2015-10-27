8ea66c3a5018afb1784f80e2916cb1ed68f5dcc9
Damon <dittopower@gmail.com>
2015-10-27 17:55:07 +1000

    Improved Auto group creation

--- 302/group/manage/index.php
+++ 302/group/manage/index.php
@@ -5,6 +5,7 @@
 	$subject = $_GET['unit'];
 	
 	function makeGroup($group){
+		Ndebug("New Group",$group);
 		global $subject;
 		$sql = "INSERT INTO `deamon_INB302`.`Groups` (`GroupName`, `GroupProject`, `UnitCode`) VALUES ('Pending', '0', '$subject')";
 		if(runSQL($sql)){
@@ -35,79 +36,88 @@
 	}
 	
 	if(isset($_POST['G_resolve'])){
-		debug("yo");
 		$result = multiSQL("SELECT `UserId`, GPA, `Similar`, `PreferenceType1`, `PreferenceType2`, `PreferenceType3` FROM `Group_Requests` NATURAL join User_Details WHERE `UnitCode` = '$subject' ORDER by User_Details.GPA desc, `PreferenceType1`, `PreferenceType2`, 'PreferenceType3'");
-		$groups = array();
-		$previous = [];
+		$fields = array();
+		$students = array();
 		while($row = mysqli_fetch_array($result,MYSQL_ASSOC)){
-			$match = 0;
+			$students[$row['UserId']] = $row;
 			debug($row);
-					debug($previous['GPA']);
-			//if($previous != ""){
-			if(in_array($previous['PreferenceType1'],$row)){
-				$match ++;debug("+1");
-			}
-			if(in_array($previous['PreferenceType2'],$row)){
-				$match ++;debug("+1");
-			}
-			if(in_array($previous['PreferenceType3'],$row)){
-				$match ++;debug("+1");
-			}
-			if($row['Similar'] == "1"){
-				if($previous['GPA'] >= ($row['GPA'] -1)){
-					$match ++;
-					debug("+1");
-				}else{
-					debug("$row[GPA] -");
-					debug($previous['GPA']);
-					$match -= ($row['GPA'] - $previous['GPA']);
-				}
-			}
-			//}
-			debug("Match:$match");
-			if($match > 1){//matching threshhold should be higher and decrease towards the formation date.
-				$groups[$previous['group']][] = $row['UserId'];
-				$row['group'] = $previous['group'];
+			if(isset($fields[$row['PreferenceType1']])){
+				$fields[$row['PreferenceType1']][] = $row['UserId'];
 			}else{
-				$groups[][] = $row['UserId'];
-				$row['group'] = count($groups)-1;
+				$fields[$row['PreferenceType1']] = array();
+				$fields[$row['PreferenceType1']][] = $row['UserId'];
 			}
-			
-			$previous = $row;
+			if(isset($fields[$row['PreferenceType2']])){
+				$fields[$row['PreferenceType2']][] = $row['UserId'];
+			}else{
+				$fields[$row['PreferenceType2']] = array();
+				$fields[$row['PreferenceType2']][] = $row['UserId'];
+			}
+			if(isset($fields[$row['PreferenceType3']])){
+				$fields[$row['PreferenceType3']][] = $row['UserId'];
+			}else{
+				$fields[$row['PreferenceType3']] = array();
+				$fields[$row['PreferenceType3']][] = $row['UserId'];
+			}
+		}
+		foreach($fields as $key=>$arrays){
+			$fields[$key] = array_unique($arrays);
 		}
 		debug("primary process done");
-		debug($groups);
+		debug($fields);
+		debug($students);
 		
-		debug("Secondary");
-		foreach($groups as $id=>$group){
-			debug($id);
-			debug($group);
-			if(count($group) > 2){//should have an import for subject minimum group size
-				debug("$id - Right size");
-				if(count($group) > 5){
-					//split the group
-					debug("Split the group");
-					$group1 = array();
-					$group2 = array();
-					for($i = 0; $i < count($group)/2;$i++){
-						$group1[] = $group[$i];
+		
+		foreach($students as $person=>$row){
+			if(isset($students[$person])){///This line is only necessary because foreach loops apparently run off the variables inital state...
+			debug($person);
+			//find people with simlar preferences
+			$ab = array_intersect($fields[$row['PreferenceType1']],$fields[$row['PreferenceType2']]);
+			$ac = array_intersect($fields[$row['PreferenceType1']],$fields[$row['PreferenceType3']]);
+			$bc = array_intersect($fields[$row['PreferenceType2']],$fields[$row['PreferenceType3']]);
+			
+			//Create one list that puts people with the most similar prefs first
+			$abc = array_merge($ab,$ac,$bc);
+			$matches = array_count_values($abc);
+			natsort($matches);
+			$matches=array_reverse($matches,true);
+			unset($matches[$person]);
+			debug($matches);
+			
+			//if there's enough similar people match a team
+			if(count($matches) > 1){
+				$group = array();
+				$group[] = $person;
+				foreach($matches as $key=>$value){
+					if($row['Similar'] == 1){//check gpa
+						if(($students[$key]['GPA']/$row['GPA']) > 0.75){
+							$group[] = $key;
+						}
+					}else{
+						$group[] = $key;
 					}
-					for($i = count($group)-1; $i > count($group)/2;$i--){
-						$group2[] = $group[$i];
+					if(count($group) > 4){//max group forming size of 5 people
+						break;
 					}
-					debug($group1);
-					debug($group2);
-					makeGroup($group1);
-					makeGroup($group2);
-				}else{
-					//form the group
-					debug("don't split");
+				}
+				if(count($group) > 1){//Make a group. minimum 2 people
+					debug($group);
+					foreach($group as $mem){
+						unset($students[$mem]);
+						foreach($fields as $key=>$value){
+							if(($me = array_search($mem, $value)) !== false) {
+								unset($fields[$key][$me]);
+							}
+						}
+					}
 					makeGroup($group);
 				}
-			}else{
-				debug("$id - wrong size");
+			}
+			Ndebug("Cycle",$students);
 			}
 		}
+		debug("secondary done");
 	}
 	
 	echo "<h1>".singleSQL("Select CONCAT(UnitCode,': ',Unit) from Unit where UnitCode='$subject'")."</h1><hr>";
